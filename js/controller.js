6a15609e45dd423fc68da09e42dd6be2413d55b1
Ashley Laisure <ashley.laisure@gmail.com>
2025-05-21 12:37:28 -0400

    randomize cards added

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,18 @@ import Elements from "./elements.js";
 class Controller {
     constructor(cardsNumber) {
         this.elements = new Elements(cardsNumber)
+        this.clickCard()
+    }
+
+    clickCard() {
+        const { attempts, cards} = this.elements
+        console.log(cards)
+        cards.forEach((card) => {
+            card.addEventListener("click", () => {
+                attempts.click++
+                card.classList.add("change")
+            })
+        })
     }
 }
 
--- js/elements.js
+++ js/elements.js
@@ -3,9 +3,18 @@ class Elements {
         this.cardsNumber = cardsNumber
         this.playground = document.querySelector('.playground')
         this.cardsIndex = []
+        this.shuffledIndex = []
         this.doubleIndex()
+        this.shuffleIndexes()
         this.createCard()
-        
+
+
+        this.cards = document.querySelectorAll(".card")
+        this.attempts = {
+            correct: 0,
+            wrong: 0,
+            click: 0,
+        }
     }
 
     createCard() {
@@ -15,7 +24,7 @@ class Elements {
         this.playground.style.gridTemplateColumns = 
             `repeat(${Math.sqrt(this.cardsNumber)}, 1fr)`
 
-        this.cardsIndex.forEach((index) => {
+        this.shuffledIndex.forEach((index) => {
             const card = document.createElement("div")
             card.classList.add("card")
             card.setAttribute("data-index", index)
@@ -29,10 +38,17 @@ class Elements {
 
     doubleIndex() {
         for(let i=1; i <= this.cardsNumber; i++) {
+            // and array with numbers one through cardsNumber/2 twice
             i <= this.cardsNumber / 2 ? this.cardsIndex.push(i) 
             : this.cardsIndex.push(i - this.cardsNumber / 2)
         }
         console.log(this.cardsIndex)
+    }
+
+    shuffleIndexes() {
+        this.shuffledIndex = this.cardsIndex.sort(() => Math.random() - 0.5)
+        console.log(this.shuffledIndex)
+        return this.shuffledIndex
     }
 
 }
